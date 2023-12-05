fd0560a28af4fccef3bc0151ad6778ec5bce74bc
Riquelme Montijo <riquelmesouzam@gmail.com>
2023-12-05 14:58:46 -0300

    Commit inicial

---
+++ src/test/java/com/example/galgodesk/GalgoDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galgodesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalgoDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
